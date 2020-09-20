152a75e71da45220f63e1f2f854787388823d543
Rodion <mark3017bess@gmail.com>
2020-09-20 13:12:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+
+/*const obj = {
+    name: 'Smit',
+    age: 20,
+    isMarried: false
+};
+*/
+
+/*let colour = prompt("Умножайте на два");
+ console.log(colour + 2);
+ */
+
+ //const answers = [];
+/*
+ answers[0] = prompt("Как ваше имя?", '');
+ answers[1] = prompt("Как ваша фамилия?", '');
+ answers[2] = prompt("Как ваше отчество?", '');
+
+ document.write(answers);
+ */
+
+//  Стркоковая интерполяция
+/*let question = prompt("Как Вас зовут?");
+ alert(`Здрайствуйте дорогой,${question}`);
+*/
+
+/*let incr = 10,
+    decr = 10;
+
+    console.log(++incr);
+    console.log(--decr);
+*/
+/*let question = +prompt("Уменшаю на один)))");
+
+console.log(--question);
+*/
+
+/*let isChecked = false,
+    isClose = false;
+
+    console.log(isChecked || !isClose);
+*/
+
+
+
+
+
